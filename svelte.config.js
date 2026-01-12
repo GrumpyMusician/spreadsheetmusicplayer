060d616996e0533c2636ddd74029a30ad2bf7620
GrumpyMusician <theverygrumpymusician@gmail.com>
2026-01-11 22:02:13 -0600

    rejiggy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html'
-		}),
-		paths: {
-			base: '/spreadsheetmusicplayer'
-		}
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: { adapter: adapter() }
 };
+
+export default config;
