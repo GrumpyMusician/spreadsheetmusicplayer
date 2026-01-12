cf65d096944f1583a027b71ecc732e7373cbc867
GrumpyMusician <theverygrumpymusician@gmail.com>
2026-01-11 21:10:44 -0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
-const repoName = 'my-sveltekit-site'; // ← CHANGE THIS
-
 export default {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html' // required for SPA routing
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? `/${repoName}` : ''
+			base: '/spreadsheetmusicplayer'
 		}
 	}
 };
