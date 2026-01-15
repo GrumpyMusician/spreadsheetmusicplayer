78b8b804ae01d8a52c57f0b1adb653bfd5a949b4
GrumpyMusician <theverygrumpymusician@gmail.com>
2026-01-15 09:17:21 -0600

    fix compiler issue

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,12 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$lib: path.resolve('./src/lib')
+		}
+	}
 };
 
 export default config;
